ace2fa1b1089ada61ba6201c53f3e0493d0274e1
shasut <30725033+shasut@users.noreply.github.com>
2021-10-24 01:26:09 +0200

    Update seed.py

--- utils/seed.py
+++ utils/seed.py
@@ -1,1 +1,13 @@
+import random, torch, os, numpy as np
 
+def my_seed(seed=42):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+my_seed()
